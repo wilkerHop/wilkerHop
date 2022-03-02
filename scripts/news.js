555a68b443b275face9ee25e1562006bcc0a8dbf
Wilker Ribeiro <rekliw97@gmail.com>
2022-03-02 17:01:16 -0300

    feat(scripts): get today news
    ... and refactor table building

--- scripts/news.js
+++ scripts/news.js
@@ -1,39 +1,43 @@
 const fs = require('fs');
 const axios = require('axios').default;
 
-const url = process.env.URL + new Date().toISOString().split('T')[0];
-console.log(url);
-const template = `<tr>
+const today = new Date();
+const yesterday = new Date(today);
+
+yesterday.setDate(yesterday.getDate() - 1);
+
+const url =
+  process.env.URL +
+  new Date().toISOString().split('T')[0] +
+  `&from=${yesterday.toISOString().split('T')[0]}`;
+
+const createArticle = (table, article) => {
+  return (
+    table +
+    `
+<tr>
 <td valign="top" align="center" width="20%">
-<a href="{{url}}">
-<span>{{sourceName}}</span>
-<img src="{{urlToImage}}" align="center" />
+<a href="${article.url}">
+<span>${article.source.name}</span>
+<img src="${article.urlToImage}" align="center" alt="${article.source.name}"/>
 </a>
 </td>
 <td valign="center" width="80%">
-{{description}}
-</td>`;
+<h2>${article.title}</h2>
+<p>${article.description}</p>
+</td>`
+  );
+};
 
 const app = async () => {
   const { data } = await axios.get(url);
 
-  console.log(data.articles.length);
-
-  const news = data.articles.slice(0, 5).map((article) => {
-    let md = template;
-
-    md = md.replace('{{url}}', article.url);
-    md = md.replace('{{sourceName}}', article.source.name);
-    md = md.replace('{{urlToImage}}', article.urlToImage);
-    md = md.replace('{{description}}', article.description);
-
-    return md;
-  });
+  const news = data.articles.slice(0, 5).reduce(createArticle, '');
 
   const readme = fs.readFileSync('README.md', 'utf8');
   const [head, , tail] = readme.split('<!-- Crypto News -->');
 
-  const table = `\n<table width="100%">\n${news.join('\n')}\n</table>\n`;
+  const table = `\n<table width="100%">${news}\n</table>\n`;
 
   const newReadme = [head, table, tail].join('<!-- Crypto News -->');
 
